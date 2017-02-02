7080ff18bd6502e7fc2515aaaec80a052e471efa
Nirobie <bob.souwer@wur.nl>
2017-02-02 16:09:21 +0100

    update main.py

--- project/main.py
+++ project/main.py
@@ -5,10 +5,10 @@ from analyze_sentiment import *
 from map_results import *
 
 ## Twitter API codes
-APP_KEY = "9jlDDVPYsigPe8Ao9UbnqjQ7Q"
-APP_SECRET = "jVHAOFrKHfpBOaJzH2K0F0ZRgB3bz5rVdlqoGQAup5tXaaspGT"
-OAUTH_TOKEN = "102152165-Sdv2opvPufyotWasY7U2C5G1ofxrckFvDtKKvTpx"
-OAUTH_TOKEN_SECRET = "FoBl3PW21BZoUeYi4WDbcUHuMC4YTAL5ZOvctMVB3eAsl"
+APP_KEY = ""
+APP_SECRET = ""
+OAUTH_TOKEN = ""
+OAUTH_TOKEN_SECRET = ""
 
 ## States of interest
 states = ['Texas','California']
